99932604c914053eb64b7c23ee0d1495e1e3b944
vokestd <vokestd@gmail.com>
2021-12-26 00:47:38 +0300

    upds

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import { Box, Center, Text } from "@chakra-ui/react";
+import { Box, Center, Heading, Image, Link, Text } from "@chakra-ui/react";
 
-import scheme from "./scheme.yaml";
+import scheme from "./scheme.json";
 
 export default function App() {
-  console.log(scheme);
   return (
     <Box>
       <Center h={"100vh"}>
-        <Text>{scheme.nickname}</Text>
+        <Box w={"30vh"}>
+          <Center>
+            <Image src={scheme.avatar} boxSize={200} borderRadius="full" />
+          </Center>
+          <Heading size={"lg"} textAlign={"center"} paddingTop={5}>
+            {scheme.nickname}
+          </Heading>
+          <Text fontSize={"xs"} textAlign={"center"}>
+            (aka {scheme.fullname})
+          </Text>
+          <Center paddingTop={8}>
+            <Link href={"https://vk.com/" + scheme.profiles.vk} isExternal>
+              <Image src={"/vk.png"} boxSize={50} />
+            </Link>
+            <Link
+              href={"https://github.com/" + scheme.profiles.github}
+              isExternal
+              paddingLeft={3}
+            >
+              <Image src={"/github.png"} boxSize={50} />
+            </Link>
+          </Center>
+        </Box>
       </Center>
     </Box>
   );
